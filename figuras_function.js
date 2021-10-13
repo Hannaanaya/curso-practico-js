08a44d63a08f5d3ca0ab0b0a9b569b1f8a3b2580
Hannaanaya <hannaanaya0511@gmail.com>
2021-10-13 18:16:32 +0200

    Formulario para área y perímetro

--- figuras_function.js
+++ figuras_function.js
@@ -48,3 +48,66 @@ function areaCirculo(radio) {
 }
 
 console.groupEnd();
+
+//HTML
+
+//Cuadrado
+function calcularPerimetroCuadrado() {
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+
+function calcularAreaCuadrado() {
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+
+    const area = areaCuadrado(value);
+    alert(area);
+}
+
+
+//Triángulo (CORREGIR)
+function calcularPerimetroTriangulo() {
+
+    const input = document.getElementById("InputTriangulo").value;
+    const value = input.value;
+
+    const lado1 = document.getElementById("lado1").value;
+    const lado2 = document.getElementById("lado2").value;
+    const base = document.getElementById("base");
+
+
+    const perimetro = perimetroTriangulo(value, lado1, lado2, base);
+    alert(perimetro);
+}
+
+
+function calcularAreaTriangulo() {
+    const input = document.getElementById("InputAreaTriangulo");
+    const value = input.value;
+
+    const perimetro = areaTriangulo(value);
+    alert(perimetro);
+}
+
+
+//Círculo
+
+function calcularPerimetroCírculo() {
+    const input = document.getElementById("InputCírculo");
+    const value = input.value;
+
+    const perimetro = perimetroCirculo(value);
+    alert(perimetro);
+}
+
+function calcularAreaCírculo() {
+    const input = document.getElementById("InputCírculo");
+    const value = input.value;
+
+    const area = areaCirculo(value);
+    alert(area);
+}
